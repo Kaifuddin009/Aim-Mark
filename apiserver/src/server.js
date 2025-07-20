628365c8dbb970789a2b67003973d94ee7eed0c9
kaifuddin009 <kaifuddin01@gmail.com>
2025-07-21 01:29:13 +0530

    final ready for deploy

--- apiserver/src/server.js
+++ apiserver/src/server.js
@@ -4,16 +4,32 @@ import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import notesRoutes from "./routes/notesRoutes.js";
 import ratelimiter from "./middleware/ratelimiter.js";
+import path from "path";
 dotenv.config();
 const app = express();
+const __dirname = path.resolve();
+
 
 app.use(express.json())//this is middleware will parse json bodies: req.body
 app.use(ratelimiter)
+
+if (process.env.NODE_ENV !== "production") {
+    
 app.use(cors({
     origin:"http://localhost:5173",
 }))
+    
+}
 
 app.use("/api/notes",notesRoutes);
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,"../front/dist")));
+
+    app.get("*",(req, res)=>{
+        res.sendFile(path.join(__dirname,"../front","dist","index.html"))
+    })
+    
+}
 
 connectDB().then(() => {
 app.listen(process.env.PORT || 5000, () =>{
--- front/src/lib/axiosInstance.js
+++ front/src/lib/axiosInstance.js
@@ -1,5 +1,7 @@
 import axios  from "axios";
+//IN production, there's no localhost so we have to make this dynamic
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5050/api" : "/api"
 const axiosInstance = axios.create({
-    baseURL:"http://localhost:5050/api"
+    baseURL:BASE_URL,
 })
 export default axiosInstance;
