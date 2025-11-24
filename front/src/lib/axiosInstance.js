e28278cc06b32d60bacd0e154e496e29ab8070b9
kaifuddin009 <kaifuddin01@email.com>
2025-11-24 19:05:48 +0530

    .

--- front/src/lib/axiosInstance.js
+++ front/src/lib/axiosInstance.js
@@ -1,6 +1,8 @@
 import axios  from "axios";
 //IN production, there's no localhost so we have to make this dynamic
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5050/api" : "/api"
+/*const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5050/api" : "/api"*/
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
 const axiosInstance = axios.create({
     baseURL:BASE_URL,
 })
