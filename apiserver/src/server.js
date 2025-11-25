f8531da5405d08a250f1ac59bfc33904fcfe16cf
kaifuddin009 <kaifuddin01@email.com>
2025-11-25 13:11:54 +0530

    .

--- apiserver/src/server.js
+++ apiserver/src/server.js
@@ -8,21 +8,10 @@ import path from "path";
 
 dotenv.config();
 const app = express();
-const __dirname = path.resolve();
 
 app.use(cors());
 app.use(express.json());
 
-// ✅ Allow frontend (both local + deployed)
-const allowedOrigins = [
-  "http://localhost:5173", // local frontend
-  "https://your-frontend.onrender.com" // deployed frontend URL
-];
-/*app.use(cors({
-  origin: allowedOrigins,  // your frontend URL
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));*/
 //Routes
 app.get("/", (req, res) => {
   res.send("Server is running ✅");
@@ -32,13 +21,13 @@ app.get("/", (req, res) => {
 app.use("/api/notes", notesRoutes);
 
 // ✅ Production: serve frontend build
-if (process.env.NODE_ENV === "production") {
+/*if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../front/dist")));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../front", "dist", "index.html"));
   });
 }
-
+*/
 connectDB().then(() => {
   app.listen(process.env.PORT || 5000, () => {
     console.log(`server is running on port ${process.env.PORT}`);
